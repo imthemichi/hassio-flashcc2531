2943f510f4bef0148a5e315a59d0eff43ec3d56a
tholmewood <34138157+tholmewood@users.noreply.github.com>
2021-09-07 12:44:07 +0100

    change compresion method

--- flashcc2531/run.sh
+++ flashcc2531/run.sh
@@ -7,7 +7,7 @@ echo "Downloading firmware"
 if ! wget https://github.com/Koenkk/Z-Stack-firmware/raw/master/coordinator/Z-Stack_Home_1.2/bin/default/CC2531_DEFAULT_20201127.zip; then echo "firmware not found" && exit 1; fi
 
 echo "unziping"
-if ! p7z e CC2531_DEFAULT_20201127.zip; then echo "unzip failed" && exit 1; fi
+if ! 7z x CC2531_DEFAULT_20201127.zip; then echo "unzip failed" && exit 1; fi
 
 echo "backup firmware"
 ./cc_read save.hex
